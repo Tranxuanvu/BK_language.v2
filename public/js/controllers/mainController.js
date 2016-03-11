c8c10fa3dc4d038387442283a9c8f16ff5ca1a38
Tranxuanvu <xuanvu442@gmail.com>
2016-03-11 15:32:54 +0700

    WIP: edit UI homepage

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,5 +1,8 @@
-angular.module('mainController', []).controller('MainController', function($scope, $sce, $location, $anchorScroll, Main) {
+angular.module('mainController', []).controller('MainController', function($scope, $sce, $location, $anchorScroll, Main, NgMap) {
   Main.loadHomeConfig().then(function (config) {
     $scope.homeConfig = config;
+  });
+  NgMap.getMap({id:"map"}).then(function(map) {       
+    map.showInfoWindow('bar', 'marker');
   });
 });
--- public/js/home.js
+++ public/js/home.js
@@ -7,3 +7,54 @@ $(document).on("scroll",function(){
     $("head").append('<link rel="stylesheet" href="../css/header-large.css" type="text/css"/>');          
   }
 });
+
+$(document).ready(function() {
+  $('html').smoothScroll(500);
+});
+
+/******************************************/
+/***************Dynamic tab***************/
+/****************************************/
+
+var tabChange = function () {
+  var tabs = $('.nav-tabs > li');
+  var active = tabs.filter('.active');
+  var next = active.next('li').length ? active.next('li').find('a') : tabs.filter(':first-child').find('a');
+  // Use the Bootsrap tab show method
+  next.tab('show');
+};
+// Tab Cycle function
+var tabCycle = setInterval(tabChange, 6000);
+
+// Tab click event handler
+$('a').on('click', function (e) {
+  e.preventDefault();
+  // Stop the cycle
+  clearInterval(tabCycle);
+  // Show the clicked tabs associated tab-pane
+  $(this).tab('show');
+  // Start the cycle again in a predefined amount of time
+  setTimeout(function () {
+    // tabCycle = setInterval(tabChange, 5000);
+  }, 36000);
+});
+
+$('a[rel=popover]').popover().click(function(e) {
+  e.preventDefault();        
+  var open = $(this).attr('data-easein');
+  if(open == 'shake') {
+    $(this).next().velocity('callout.' + open);
+  } else if(open == 'pulse') {
+    $(this).next().velocity('callout.' + open);
+  } else if(open == 'tada') {
+    $(this).next().velocity('callout.' + open);
+  } else if(open == 'flash') {
+    $(this).next().velocity('callout.' + open);
+  }  else if(open == 'bounce') {
+   $(this).next().velocity('callout.' + open);
+  } else if(open == 'swing') {
+   $(this).next().velocity('callout.' + open);
+  }else {
+   $(this).next().velocity('transition.' + open); 
+  }          
+});
