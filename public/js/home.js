8e6fe70f99e03e24faa164a71d4ed55bf72b05a5
Tranxuanvu <xuanvu442@gmail.com>
2016-03-13 02:26:26 +0700

    edit icon in sidebar and add form login-register

--- public/js/home.js
+++ public/js/home.js
@@ -57,17 +57,57 @@ $('a[rel=popover]').popover().click(function(e) {
 
 /******************************************/
 /***************Popup Modal***************/
-/****************************************/// 
+/****************************************/
 var ModalEffects = (function() {
 
   function init() {
 
-    var overlay = document.querySelector( '.md-overlay' );
+    /*----------------login----------------*/
+    var overlayLoginRes = document.querySelector( '.login-register .md-overlay' );
 
-    [].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+    [].slice.call( document.querySelectorAll( '.login-register .md-trigger' ) ).forEach( function( el, i ) {
 
       var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
-        close = modal.querySelector( '.md-close' );
+        closeLoginRes = modal.querySelector( '.login-register .md-close' );
+
+      function removeModal( hasPerspective ) {
+        classie.remove( modal, 'md-show' );
+
+        if( hasPerspective ) {
+          classie.remove( document.documentElement, 'md-perspective' );
+        }
+      }
+
+      function removeModalHandler() {
+        removeModal( classie.has( el, 'md-setperspective' ) ); 
+      }
+
+      el.addEventListener( 'click', function( ev ) {
+        classie.add( modal, 'md-show' );
+        overlayLoginRes.removeEventListener( 'click', removeModalHandler );
+        overlayLoginRes.addEventListener( 'click', removeModalHandler );
+
+        if( classie.has( el, 'md-setperspective' ) ) {
+          setTimeout( function() {
+            classie.add( document.documentElement, 'md-perspective' );
+          }, 25 );
+        }
+      });
+
+      closeLoginRes.addEventListener( 'click', function( ev ) {
+        ev.stopPropagation();
+        removeModalHandler();
+      });
+
+    } );
+
+    /*----------------sidebar----------------*/
+    var overlay = document.querySelector( '#sidebar .md-overlay' );
+
+    [].slice.call( document.querySelectorAll( '#sidebar .md-trigger' ) ).forEach( function( el, i ) {
+
+      var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
+        close = modal.querySelector( '#sidebar .md-close' );
 
       function removeModal( hasPerspective ) {
         classie.remove( modal, 'md-show' );
@@ -106,3 +146,49 @@ var ModalEffects = (function() {
 
 })();
 
+/******************************************/
+/***************Form login/register*******/
+/****************************************/
+$('#navbar-top .md-content').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+      if( $this.val() === '' ) {
+        label.removeClass('active highlight'); 
+      } else {
+        label.removeClass('highlight');   
+      }   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+        label.removeClass('highlight'); 
+      } 
+      else if( $this.val() !== '' ) {
+        label.addClass('highlight');
+      }
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
