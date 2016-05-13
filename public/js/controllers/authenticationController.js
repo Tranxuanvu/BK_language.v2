a7fb91776a0fb0b045c4b5048805af1faf459385
Tranxuanvu <xuanvu442@gmail.com>
2016-05-13 15:18:32 +0700

    Remove footer

--- public/js/controllers/authenticationController.js
+++ public/js/controllers/authenticationController.js
@@ -19,8 +19,6 @@ angular.module('authenticationController', []).filter('split', function() {
     $scope.currentUser = user.email;
   } else {
     $scope.login = function () {
-
-      console.log('Login');
       
       $scope.user.password = md5.createHash($scope.user.password);
       
